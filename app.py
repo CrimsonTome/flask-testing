67a300a26013bddd117a54d7c4d6cd5f0baa336f
CrimsonTome <crimsontome427@protonmail.com>
2022-12-13 09:16:21 +0000

    chore: add db handling with sqlite

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template, redirect, url_for, request, session,flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash, g
 from functools import wraps
+import sqlite3
 
 # create the application object
 app = Flask(__name__)
 
 app.secret_key = "some key"
+app.database = 'sample.db'
+
 
 # login required decorator
 def login_required(f):
@@ -21,7 +24,12 @@ def login_required(f):
 @app.route('/')
 @login_required
 def home():
-    return render_template("index.html")
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template("index.html", posts=posts)
+
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html')  # render a template
@@ -46,6 +54,10 @@ def logout():
     session.pop("logged_in", None)
     flash("You were just logged out")
     return redirect(url_for("welcome"))
+
+# connect to database
+def connect_db():
+    return sqlite3.connect(app.database)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
